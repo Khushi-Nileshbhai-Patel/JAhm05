72c9d90a67748ad9fd79c62e00caa0c328911536
Khushi-Nileshbhai-Patel <khuahimili4212@gmail.com>
2024-06-03 08:48:01 +0530

    Add files via upload

---
+++ AreaOfCircleRectangleTriangle/main/area/Area.java
@@ -0,0 +1,16 @@
+package area;
+
+public class Area {
+	private double length;
+    private double breadth;
+
+    public void setDim(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double getArea() {
+        return length * breadth;
+    }
+
+}
