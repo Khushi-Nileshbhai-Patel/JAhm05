b793968eca80afec9acbdf26e0c96aeec33a34ba
Khushi-Nileshbhai-Patel <khuahimili4212@gmail.com>
2024-06-07 10:38:45 +0530

    Add files via upload

---
+++ ExceptionHandling/throwexample/Main.java
@@ -0,0 +1,12 @@
+package ExceptionHandling.throwexample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ThrowExample te = new ThrowExample();
+		te.setMarks(-1);
+		System.out.println(te.getMarks());
+	}
+
+}
